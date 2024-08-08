3199f2b0be00c264accb8fd73607c55c125295a8
imoleFolio <dremkay71@gmail.com>
2024-08-08 11:55:55 +0100

    task5: comment aded

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,6 @@
+-- Write a SQL script that creates a trigger that resets the attribute valid_email
+-- only when the email has been changed.
 /*
-Write a SQL script that creates a trigger that resets the attribute valid_email
-only when the email has been changed.
-
 Context: Nothing related to MySQL, but perfect for user email validation - 
 distribute the logic to the database itself!
 */
