87618b87147289dbda07bd5d281fe4935730a4ec
imoleFolio <dremkay71@gmail.com>
2024-08-13 12:29:58 +0100

    task 101 import removed

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -7,10 +7,9 @@ mongo_collection will be the pymongo collection object
 The top must be ordered
 The average score must be part of each item returns with key = averageScore
 """
-from pymongo.collection import Collection
 
 
-def top_students(mongo_collection: Collection):
+def top_students(mongo_collection):
     """returns all students sorted by average score"""
     return mongo_collection.aggregate(
         [
